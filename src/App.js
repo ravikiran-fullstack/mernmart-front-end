54112e8ab5f612291081e30c544b93d98311ac75
ravikiran <ravikiransjce.jobs@gmail.com>
2022-03-10 09:35:07 +0530

    Added two routes, home and product screens pages, and moved App content to HomeScreen page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import data from "./data";
 import "./App.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href='/'>MERNMART</a>
-      </header>
-      <main>
-        <h1>Featured products</h1>
-        <div className='products'>
-          {data.products.map((product) => {
-            return (
-              <div className='product' key={product.slug}>
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className='productInfo'>
-                  <a href={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add to Cart</button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to='/'>MERNMART</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route
+              path='/product/:slug'
+              element={<ProductScreen></ProductScreen>}
+            ></Route>
+            <Route path='/' element={<HomeScreen></HomeScreen>}></Route>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
